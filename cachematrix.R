860c409d3b84f0ff4d06630bc8426354a025f064
Henry Bottomley <se16@btinternet.com>
2014-04-17 01:36:35 +0100

    Expand skeleton into working code with comments and example

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two R functions able to cache potentially time-consuming 
+## computations: in this particular case matrix inversion.
 
-## Write a short comment describing this function
+## The first function makeCacheMatrix() 
+## sets up the cache but does not calculate the inverse 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(m = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                m <<- y
+                inv <<- NULL
+               }
+        get <- function() m
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv 
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+       }
 
-}
 
+## The second function cacheSolve() 
+## takes the matrix, checks if there is an inverse cached,
+## and if so then returns it, but if not calculates it and caches it
+## all assuming that the matrix is invertible
 
-## Write a short comment describing this function
+cacheSolve <- function(m, ...) {
+        ## Return a matrix that is the inverse of 'm'
+        inv <- m$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+               }
+        data <- m$get()
+        inv <- solve(data, ...)
+        m$setinverse(inv)
+        inv 
+       }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+## Example to illustrate use (remove # at beginning of lines)
+#mymat <- rbind(c(0,2),c(1,3))  ## inverse is rbind(c(-1.5,1),c(0.5,3))
+#cmat <- makeCacheMatrix(mymat) ## set up on mymat but do not calculate yet
+#cacheSolve(cmat)               ## nothing cached so calculate and cache 
+#cacheSolve(cmat)               ## this time inverse cached so use cache
